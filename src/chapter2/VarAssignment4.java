f494ebeee642b768fffbe945f7c4a9698f80e3c7
donghyun-0503 <donghyun100503@gmail.com>
2026-08-12 09:52:28 +0900

    자바 기초

---
+++ src/chapter2/VarAssignment4.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class VarAssignment4 {
+
+  public static void main(String[] args) {
+    byte a = 10;
+    short b = 200;
+    // byte c = 150; byte의 표현 범위인 128을 넘어감
+    int d = 300;
+    // long e = 5000000000; 뒤에 L을 안붙임
+    // float f = 5.7; 뒤에 f를 안붙임
+    double g = 5.7;
+    boolean h = true;
+    char i = 'A';
+  }
+}
