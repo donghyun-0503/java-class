c05af94cb97795aae64b3cca33ae6e309337fdfe
donghyun-0503 <donghyun100503@gmail.com>
2026-09-09 09:47:51 +0900

    자바 기초

---
+++ src/constructor/Person.java
@@ -0,0 +1,19 @@
+package constructor;
+
+public class Person {
+    String name;
+    float height;
+    float weight;
+
+    public Person() {};
+
+    public Person(String pName) {
+        name = pName;
+    };
+
+    public Person(String pName, float pHeight, float pWeight) {
+        name = pName;
+        height = pHeight;
+        weight = pWeight;
+    }
+}
